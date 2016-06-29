aeb1870bd93925bbb3f219fe21036d472b776c0d
Kenneth Ede <9659@internet.nt.avs>
2016-06-29 16:59:49 +0100

    Add test preparation

---
+++ PatientsAndEpisodes/RestApi/IoC/AutofacInit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.IoC
+{
+    public class AutofacInit
+    {
+    }
+}
---
+++ PatientsAndEpisodes/UnitTests/IoC/AutofacTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace RestApiUnitTests.IoC
+{
+    [TestFixture]
+    public class AutofacTests
+    {
+        [Test]
+        public void ResolveController_UsingDependencyContainer_ExpectPatientsControllerAndDependencies()
+        {
+            // arrange
+
+            // act
+            var actual = 1; //RestApi.IoC.AutofacInit();
+
+            // assert
+            Assert.IsInstanceOf(typeof(RestApi.Controllers.PatientsController), actual);
+        }
+    }
+}
